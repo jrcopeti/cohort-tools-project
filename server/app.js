e03cd46ea7ac514d86169c3f4a7af725c92573e0
Jose Copeti <jrcopeti@gmail.com>
2024-07-30 17:36:03 +0200

    implemented mongoose

--- server/app.js
+++ server/app.js
@@ -2,16 +2,29 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
+
+//Mongoose
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
+
+// MODELS
+const Cohort = require("./models/cohort");
+console.log("Cohort", Cohort);
+const Student = require("./models/student.js");
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -30,8 +43,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-// ...
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -41,12 +52,23 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+app.get("/api/cohorts", async (req, res) => {
+  try {
+    const cohorts = await Cohort.find();
+    console.log("cohorts", cohorts);
+    res.json(cohorts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get("/api/students", (req, res) => {
-  res.json(students);
+app.get("/api/students", async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // START SERVER
